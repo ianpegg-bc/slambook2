f9925fc7f86efe940fee82db19ac5389455e05ba
Ian Pegg <ian.pegg@braincorp.com>
2022-10-22 20:05:28 -0700

    edits

--- 3-eigen/useGeometry/eigenGeometry.cpp
+++ 3-eigen/useGeometry/eigenGeometry.cpp
@@ -8,14 +8,13 @@ using namespace std;
 
 using namespace Eigen;
 
-// 本程序演示了 Eigen 几何模块的使用方法
+/** 
+ * Demonstration of the Eigen geometry module
+ */
 
 int main(int argc, char **argv) {
 
-  // Eigen/Geometry 模块提供了各种旋转和平移的表示
-  // 3D 旋转矩阵直接使用 Matrix3d 或 Matrix3f
   Matrix3d rotation_matrix = Matrix3d::Identity();
-  // 旋转向量使用 AngleAxis, 它底层不直接是Matrix，但运算可以当作矩阵（因为重载了运算符）
   AngleAxisd rotation_vector(M_PI / 4, Vector3d(0, 0, 1));     //沿 Z 轴旋转 45 度
   cout.precision(3);
   cout << "rotation matrix =\n" << rotation_vector.matrix() << endl;   //用matrix()转换成矩阵
