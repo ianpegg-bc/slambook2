2588787ef76a82b6f853d28355893d1eed1c1914
Ian Pegg <ian.pegg@braincorp.com>
2022-10-22 21:09:39 -0700

    update

--- 4-sophus/useSophus.cpp
+++ 4-sophus/useSophus.cpp
@@ -15,44 +15,42 @@ int main(int argc, char **argv) {
   Matrix3d R = AngleAxisd(M_PI / 2, Vector3d(0, 0, 1)).toRotationMatrix();
   // or quaternion
   Quaterniond q(R);
-  Sophus::SO3d SO3_R(R);              // Sophus::SO3d可以直接从旋转矩阵构造
-  Sophus::SO3d SO3_q(q);              // 也可以通过四元数构造
+  Sophus::SO3d SO3_R(R);              // construction from a rotation matrix
+  Sophus::SO3d SO3_q(q);              // construction from a quaternion
   // 二者是等价的
   cout << "SO(3) from matrix:\n" << SO3_R.matrix() << endl;
   cout << "SO(3) from quaternion:\n" << SO3_q.matrix() << endl;
   cout << "they are equal" << endl;
 
-  // 使用对数映射获得它的李代数
+  // Lie algebra via the logarithmic map
   Vector3d so3 = SO3_R.log();
   cout << "so3 = " << so3.transpose() << endl;
-  // hat 为向量到反对称矩阵
+  // hat converts from a vector to a skew-symmetric matrix
   cout << "so3 hat=\n" << Sophus::SO3d::hat(so3) << endl;
-  // 相对的，vee为反对称到向量
+  // vee converts from a skew symmetric-matrix to a vector
   cout << "so3 hat vee= " << Sophus::SO3d::vee(Sophus::SO3d::hat(so3)).transpose() << endl;
 
-  // 增量扰动模型的更新
-  Vector3d update_so3(1e-4, 0, 0); //假设更新量为这么多
+  // perturb by a small amount
+  Vector3d update_so3(1e-4, 0, 0);
   Sophus::SO3d SO3_updated = Sophus::SO3d::exp(update_so3) * SO3_R;
   cout << "SO3 updated = \n" << SO3_updated.matrix() << endl;
 
   cout << "*******************************" << endl;
-  // 对SE(3)操作大同小异
-  Vector3d t(1, 0, 0);           // 沿X轴平移1
-  Sophus::SE3d SE3_Rt(R, t);           // 从R,t构造SE(3)
-  Sophus::SE3d SE3_qt(q, t);            // 从q,t构造SE(3)
+  // Similarly for SE(3)
+  Vector3d t(1, 0, 0);           // translation 1 along the X direction
+  Sophus::SE3d SE3_Rt(R, t);           // Construct SE(3) from R, t
+  Sophus::SE3d SE3_qt(q, t);            // from q, t
   cout << "SE3 from R,t= \n" << SE3_Rt.matrix() << endl;
   cout << "SE3 from q,t= \n" << SE3_qt.matrix() << endl;
-  // 李代数se(3) 是一个六维向量，方便起见先typedef一下
+  // define a type for the lie algebra se(3)
   typedef Eigen::Matrix<double, 6, 1> Vector6d;
   Vector6d se3 = SE3_Rt.log();
   cout << "se3 = " << se3.transpose() << endl;
-  // 观察输出，会发现在Sophus中，se(3)的平移在前，旋转在后.
-  // 同样的，有hat和vee两个算符
   cout << "se3 hat = \n" << Sophus::SE3d::hat(se3) << endl;
   cout << "se3 hat vee = " << Sophus::SE3d::vee(Sophus::SE3d::hat(se3)).transpose() << endl;
 
-  // 最后，演示一下更新
-  Vector6d update_se3; //更新量
+  // update in se(3)
+  Vector6d update_se3;
   update_se3.setZero();
   update_se3(0, 0) = 1e-4;
   Sophus::SE3d SE3_updated = Sophus::SE3d::exp(update_se3) * SE3_Rt;
